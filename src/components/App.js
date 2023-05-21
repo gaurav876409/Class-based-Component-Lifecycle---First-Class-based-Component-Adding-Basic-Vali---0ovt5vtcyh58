4c4ccd3049d105f7227bd634875bd598d2865704
gaurav876409 <gaurav876409@gmail.com>
2023-05-21 09:17:22 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -6,21 +6,22 @@ import '../styles/App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    this.state = { date: new Date() };
   }
 
   componentDidMount() {
-    this.timerID = setInterval(
-// write your code here
- 
-    );
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
   }
 
   tick() {
     this.setState({
       date: new Date()
     });
-    
+
   }
 
   render() {
